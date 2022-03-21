cc7a482c940f37c06dd56d6086b4cf3e7811766d
ganggaswadiaya <ganggaswadiaya@gmail.com>
2022-03-21 20:17:22 +0800

    change file name of the index.html into index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
-const link = dokumnet.getElementsByClassName("link")[0];
+const linkMenu = dokumnet.getElementsByClassName("link")[0];
 
 toggleMenu.addEventListener("click", () => {
-  link.classList.toggle("");
+  link.classList.toggle("active");
 });
